e5c290d643f33506d6ee2c33cf797c1008b6a458
osifraga <a00835812@tec.mx>
2025-04-22 16:50:00 -0600

    SpeedInsightFix

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar';
 import Header from './Header';
 import styles from './Layout.module.css'; // Importa los estilos
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { SidebarProvider } from '../../context/SidebarContext';
 
 function Layout({ children }) {
@@ -17,7 +17,7 @@ function Layout({ children }) {
           <main className={styles.pageContent}>
             {children} {/* Aquí se renderizará el contenido de cada pantalla */}
             <Analytics />
-            <SpeedInsights />
+            <SpeedInsights /> {/* Añadido el componente SpeedInsights */}
           </main>
         </div>
       </div>
